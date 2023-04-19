c18b887d4a24c7449e480d88d5f53455a1f27b17
Tirth-021 <125239162+Tirth-021@users.noreply.github.com>
2023-04-19 16:53:11 +0530

    Initial commit

---
+++ quiz/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+#@login_required
+def home(request):
+    # global flag
+    # print(flag)
+    # if flag==2:
+    #     flag-=1
+    #     print(flag)
+    # print(flag)
+    return render(request, "home.html")
+
+def index(request):
+    return render(request, "index.html")
+
+def logout_view(request):
+    logout(request)
+    print("Logout successful")
+    return render(request,"home.html")
+
+
